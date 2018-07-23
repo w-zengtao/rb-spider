3e0cb3eb282f72c6f70235deb86da8edfcb1c633
w-zengtao <so.zengtao@gmail.com>
2018-07-23 16:46:43 +0800

    [TODO] WriteToMiddleware Must include Rabbitmq

--- lib/spider/fetcher.rb
+++ lib/spider/fetcher.rb
@@ -13,8 +13,12 @@ module Spider
     private
       def parse_resp(resp)
         code, headers, body = resp.code, resp.headers, resp.body
+        if Scheduler.hget(@url) == Digest::MD5.hexdigest(body)
 
-        puts Digest::MD5.hexdigest(body)
+        else
+          Scheduler.set(@url, Digest::MD5.hexdigest(body))
+          write_to_middleware
+        end
       end
 
       def write_to_middleware
--- lib/spider/scheduler.rb
+++ lib/spider/scheduler.rb
@@ -1,44 +1,51 @@
 # Scheduler
-# 调度器，这里每分钟从池里拿取等待请求的数据
+# 调度器，这里池里拿取等待请求的数据 & 分发处理抓取的数据
 
 require "redis"
 
 module Spider
   module Scheduler
-    class << self
-      def redis 
-        @redis ||= Redis.new(
-          host: Config.instance.config["redis"]["url"],
-          port: Config.instance.config["redis"]["port"],
-          db:   Config.instance.config["redis"]["db"]
-        )
-      end
 
-      #  这里是执行的入口
-      def exec
-        pools.each do |url|
-          Fetcher.new(url).exec
-        end
-      end
+    SCHEDULED_POOLS = "scheduled_pools"     # 调度池
+    CTAG_HASH       = "ctag_hash"           # 这里存储爬虫上一次数据的签名
 
-      def pools
-        pendings = redis.smembers("scheduled_pools")
-        clean
-        return pendings
-      end
-
-      # 每次有数据进来的时候自动调用 & url 是 Array 格式
-      def add(url = [])
-        return if url.empty?
-        redis.sadd("scheduled_pools", url)
-        exec
-      end
-
-      private
-        def clean 
-          redis.del("scheduled_pools")
-        end
+    def self.redis
+      @redis ||= Redis.new(
+        host: Config.instance.config["redis"]["url"],
+        port: Config.instance.config["redis"]["port"],
+        db:   Config.instance.config["redis"]["db"]
+      )
     end
+
+    def self.exec
+      pools.each { |url| Fetcher.new(url).exec }
+    end
+
+    def self.pools
+      pendings = redis.smembers(SCHEDULED_POOLS)
+      clean
+      return pendings
+    end
+
+    def self.add(url = [])
+      return if url.empty?
+      redis.sadd(SCHEDULED_POOLS, url)
+      exec
+    end
+
+    # 这里判断是否需要进入数据处理流程
+    def self.hset(url, md5ed = nil)
+      redis.hset(CTAG_HASH, Digest::MD5.hexdigest(url), md5ed)
+    end
+
+    def self.hget(url)
+      redis.hget(CTAG_HASH, Digest::MD5.hexdigest(url))
+    end
+
+    private
+      def self.clean
+        redis.del(SCHEDULED_POOLS)
+      end
   end
 end
 
